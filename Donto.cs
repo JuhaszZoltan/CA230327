96b926eb1adf7b72fa701d788e3a9a9e0e84c81e
Juhász Zoltán <juhaszzoltanmate@yahoo.com>
2023-03-27 15:13:26 +0200

    Add project files.

---
+++ Donto.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA230327
+{
+    internal class Donto
+    {
+        public RomaiSorszam Ssz { get; set; }
+        public DateTime Datum { get; set; }
+        public string Gyoztes { get; set; }
+        public string Eredmeny { get; set; }
+        public string Vesztes { get; set; }
+        public string Helyszin { get; set; }
+        public string VarosAllam { get; set; }
+        public int Nezoszam { get; set; }
+        public int GyoztesPont { get; private set; }
+        public int VesztesPont { get; private set; }
+
+        public Donto(string s)
+        {
+            string[] v = s.Split(';');
+            Ssz = new RomaiSorszam(v[0]);
+            Datum = DateTime.Parse(v[1]);
+            Gyoztes = v[2];
+            Eredmeny = v[3];
+            string[] w = v[3].Split('-');
+            GyoztesPont = int.Parse(w[0]);
+            VesztesPont = int.Parse(w[1]);
+            Vesztes = v[4];
+            Helyszin = v[5];
+            VarosAllam = v[6];
+            Nezoszam = int.Parse(v[7]);
+        }  
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using CA230327;
+using System.Text;
+
+List<Donto> dontok = new List<Donto>();
+StreamReader sr = new StreamReader(
+    "..\\..\\..\\res\\SuperBowl.txt", Encoding.UTF8);
+_ = sr.ReadLine();
+while (!sr.EndOfStream)
+{
+    dontok.Add(new Donto(sr.ReadLine()));
+}
+
+Console.WriteLine($"4. feladat: döntők száma: {dontok.Count}");
+
+int osszkulonbeg = 0;
+foreach  (var d in dontok)
+{
+    osszkulonbeg += (d.GyoztesPont - d.VesztesPont);
+}
+float atlKul = osszkulonbeg / (float)dontok.Count;
+Console.WriteLine($"5. feladat: átlagos pontkülönbség: {atlKul:0.0}");
+
+int maxi = 0;
+for (int i = 1; i < dontok.Count; i++)
+{
+    if (dontok[i].Nezoszam > dontok[maxi].Nezoszam)
+    {
+        maxi = i;
+    }
+}
+Console.WriteLine("legmagasabb nézőszám a döntők során:\n" +
+    $"\tSorszám (dátum): {dontok[maxi].Ssz.ArabSsz} ({dontok[maxi].Datum.ToShortDateString()})\n" +
+    $"\tGyőztes csapat: {dontok[maxi].Gyoztes}, szerzett pontok: {dontok[maxi].GyoztesPont}\n" +
+    $"\tVesztes csapat: {dontok[maxi].Vesztes}, szerzett pontok: {dontok[maxi].VesztesPont}\n" +
+    $"\tHelyszín: {dontok[maxi].Helyszin}\n" +
+    $"\tVáros, Állam: {dontok[maxi].VarosAllam}\n" +
+    $"\tNézőszám: {dontok[maxi].Nezoszam}");
+
